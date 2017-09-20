ee90af4ed51bc1f2dda8ebaecb799f1920af3485
Felix <ff@rapidfacture.com>
2017-09-20 23:06:55 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,117 @@
+# rf-load
+
+* simple asynchron NodeJS module loading lib
+* load single files, node modules, functions, or a complete directory
+* all modules are collected in a list, then required and started one by one
+* modules are kept in memory and can be accessed with a require() like function at any time
+
+## Getting Started
+
+> npm install rf-load
+
+```js
+var load = new(require("rf-load").moduleLoader)();
+
+// get the modules
+
+//files
+   load.setModulePath("moduledir");
+   load.file("db"); // load moduledir/db.js
+   load.file("acl", { parameters: 123 }); // additional parameters
+
+// npm module
+   load.module("web", { parameters: 123 });
+
+// function
+   load.func(function (options, next) {
+      console.log("I am a function " + options.whatami);
+      next();
+   }, { whatami: "without name" } ); // load a function
+
+
+// start modules one by one
+load.startModules();
+```
+
+
+Or load all files in a directory parallel:
+```js
+load.getDirectoryPaths('./server/api').forEach(function(path){
+   // start each file with specific arguments
+   require(path).start(dataBase, app);
+});
+
+// place the above code in the module loader,
+// if it shoud be executed, after other module were loaded:
+load.func(function (options, next) {
+   load.getDirectoryPaths('./server/api').forEach(function(path){
+      require(path).start(dataBase, app);
+   }); next();
+});
+
+
+```
+
+
+## Module structure
+
+* 'start' function necessary
+*  the start function calls the next module. If it fails, the module execution will not finish.
+
+```js
+"use strict";
+
+module.exports.start = function (options, next) {
+   // ...do your module things
+
+   // call the next module
+   next();
+}
+```
+
+## Accessing modules from other files
+
+* modules can be accessed after loading with a special require function
+* intention: main module loads and configures all required modules
+* similar ro `require()`, but it throws an error if the module hasn't been loaded
+
+
+```js
+var acl = require("rf-load").require("acl");
+// You can now access any module.exports variables and functions from the acl module
+```
+
+## Conflicting module names
+
+If you ever run into the issue of having modules of the same base name, that would later on cause confusion on your .require() calls, you can define a prefix that is added in front of each modules name.
+
+For example, imagine two module directories:
+* moddir1
+  * mod1.js
+  * mod2.js
+* moddir2
+  * mod1.js
+  * mod2.js
+
+Now include them as follows:
+
+```js
+var load1 = new(require("rf-load").moduleLoader)("dir1.");
+load1.moduleDirectory("moddir1");
+load1.startModules();
+
+var load2 = new(require("rf-load").moduleLoader)("dir2.");
+load2.moduleDirectory("moddir2");
+load2.startModules();
+```
+
+Later on, you can require them individually with their prefix:
+```js
+var moddir1_mod2 = require("rf-load").require("dir1.mod2"),
+    moddir2_mod1 = require("rf-load").require("dir2.mod1");
+```
+
+
+## Legal Issues
+* Licenese: MIT
+* Author: Julian von Mendel
---
+++ index.js
@@ -0,0 +1,164 @@
+// rf-load initializes rapidfacture and node modules
+/* jshint node: true */ "use strict";
+
+var fs = require("fs");
+var exp = module.exports;
+
+// error handling
+var logError = function(err){
+   throw new Error(console.log(err));
+};
+try { // try using rf-log
+   var critical = require(require.resolve("rf-log")).critical;
+   if(critical) logError = critical;
+} catch (e) {}
+
+
+
+exp.modules = {};
+
+
+
+exp.require = function (module) {
+   if (exp.modules[module] === undefined) {
+      logError("Module '" + module + " wasn't loaded yet! Make sure to include it in your load module process.");
+   }
+   if (typeof exp.modules[module].require === "string") {
+      logError("You are using a forward dependency for module '" + module + "'. Fix the order of your module inclusions or avoid this dependency.");
+   }
+   return exp.modules[module];
+};
+
+
+
+exp.moduleLoader = function (modulePrefix) {
+   modulePrefix = ((typeof modulePrefix == "string") ? modulePrefix : "");
+
+   // Init
+   var modulepath = ".";
+   var setModulePath = function (path) {
+      modulepath = path;
+   };
+
+
+   // Load module file path for the first time
+   function loadFile(module, options, useBasePath) {
+      var path = ((useBasePath) ? "" : modulepath);
+      return loadModule(path + "/" + module + ".js", options);
+   }
+
+   // Load module for the first time (NPM or file path)
+   function loadModule(module, options) {
+      // ensure module name contains no directory path or a file extension
+      var name = module.split(/[\\/]/).pop().replace(/\.[^/.]+$/, "");
+      return loadFunction(null, options, name, { require: module });
+   }
+
+   // Load function
+   var moduleoptions = {}, modulelist = [], unnamedModuleCounter = 0;
+   function loadFunction(func, options, modulename, obj) {
+      if (typeof modulename != 'string') {
+         // Function without a name. Create one.
+         modulename = "unnamed." + (unnamedModuleCounter++);
+      }
+      modulename = modulePrefix + modulename;
+
+      //log.info("Mod '"+modulename+"' loaded."));
+      exp.modules[modulename] = ((typeof obj == 'object') ? obj : { start: func });
+      moduleoptions[modulename] = ((typeof options == 'object') ?
+            options : {});
+
+      // Register module for loading
+      modulelist.push(modulename);
+
+      return true;
+   }
+
+   // Start modules
+   var modulePosition = 0; // How many modules have been worked off so far?
+   function startModules (noclear) {
+      if (!noclear) {
+         loadFunction(function (options, next) {
+            modulelist = [];
+            modulePosition = 0;
+            moduleoptions = {};
+         }, null, '__clear');
+      }
+
+      function next() {
+         var module = modulelist[modulePosition++];
+         //log.info("Executing module '" + module + "' (position " + modulePosition);
+
+         if (typeof exp.modules[module].require === "string") {
+            try {
+               exp.modules[module] = require(require.resolve(
+                        exp.modules[module].require));
+            } catch(e) {
+               logError("Error loading module '" + module + "': " + e.message);
+               return false;
+            }
+         }
+
+         if (typeof exp.modules[module].start !== "function") {
+            logError("Module '" + module + "' has no start() function!");
+            return false;
+         }
+
+         exp.modules[module].start(moduleoptions[module], next);
+      }
+      next();
+   }
+
+
+   // Load all modules in a list serially
+   function loadModules(modules, useBasePath) {
+      modules.forEach(function (m) {
+
+         if (typeof m == 'string')  loadFile(m, {}, useBasePath);
+
+         if (typeof m != 'object') return;
+
+         if (m.type == 'file') loadModule(m.file, m.options, useBasePath);
+
+         if (m.type == 'npm')  loadModule(m.name, m.options);
+
+         if (m.type == 'function') loadFunction(m.func, m.options, m.name, m.obj);
+
+      });
+      return true;
+   }
+
+   // Load all modules from a directory in parallel
+   function loadModuleDirectory (path) {
+      return loadModules(getDirectoryPaths(path), true);
+   }
+
+
+   function getDirectoryPaths(path){
+      var pathList = [];
+      fs.readdirSync(path).forEach(function(file) {
+         var filePath = path + '/' + file;
+         var stat = fs.statSync(filePath);
+
+         if (stat && stat.isDirectory()) {
+           pathList = pathList.concat(getDirectoryPaths(filePath));
+         } else if (file[0] != '.') {
+           pathList.push(path + '/' + file.split(".")[0]);
+         }
+      });
+      return pathList;
+   }
+
+
+   return {
+      setModulePath: setModulePath,
+      //loadModules: loadModules,
+      moduleDirectory: loadModuleDirectory,
+      startModules: startModules,
+      getDirectoryPaths: getDirectoryPaths,
+
+      file: loadFile,
+      module: loadModule,
+      func: loadFunction,
+   };
+};
